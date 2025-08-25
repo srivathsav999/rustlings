729ee2cd9f5606da8185537bf16d00b366166007
Srivathsav-Kyatham <2023.srivathsavk@isu.ac.in>
2025-08-25 22:41:55 +0530

    finallly solved this fking thing

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,13 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let team = scores.entry(team_1_name).or_default();
+        team.goals_scored += team_1_score;
+        team.goals_conceded += team_2_score;
+
+        let team = scores.entry(team_2_name).or_default();
+        team.goals_scored += team_2_score;
+        team.goals_conceded += team_1_score;
     }
 
     scores
